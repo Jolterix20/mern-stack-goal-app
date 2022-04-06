f1ce490ad8a53f4ff8a89808d07333d2e51aa8bf
Jolterix20 <jayeshvbane@gmail.com>
2022-04-06 12:40:22 +0530

    Error handling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 // Importing routes
 const goalRoutes = require('./routes/goalRoutes')
@@ -9,8 +10,15 @@ const PORT = process.env.PORT || 5000
 // Creating Express App
 const app = express()
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // Routes middleware
 app.use('/api/goals', goalRoutes)
+
+// Overriding default error handler
+app.use(errorHandler)
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
